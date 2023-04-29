e26e20e55c1bcbdb65ff5307af75add468410002
ANGEL0CADUTO <114922069+ANGEL0CADUTO@users.noreply.github.com>
2023-04-29 23:14:42 +0200

    Add files via upload

--- Tutoraggio 4/EsercizioTut4.c
+++ Tutoraggio 4/EsercizioTut4.c
@@ -9,51 +9,59 @@
 
 
 int i;
-pthread_t threadId1;/*inizializziamo la variabile thread che ci servirà poi per creare il thread vero e proprio*/
+pthread_t threadId1;/*inizializziamo la variabile che identificherà il nostro thread */
 pthread_t threadId2;
 pthread_t threadId3;
 pthread_t threadId4;
-pthread_t * array[] = {&threadId1,&threadId2,&threadId3,&threadId4};
+pthread_t * array[] = {&threadId1,&threadId2,&threadId3,&threadId4}; //ci servirà per farci i cicli for sopra
 cpu_set_t cpuset; /*maschera di 64 bit */
 pthread_attr_t threadAttr; /* piu tardi voglio capire come usare USARE PTHREAD_ATTR_SETAFFINITY*/
 
-void *thread_function(void* arg); /*inizializziamo la funzione che daremo in pasto alla funzione di creazione del thread*/
+
+void *thread_function(void* arg); /*definiamo la funzione che i threads eseguiranno*/
+
 
 int main(int argc,char*argv[]){
-    printf("numero CPU : %lu\n ",sizeof(CPU_COUNT(&cpuset)));
-    printf("INIZIO PROGRAMMA: creazione threads a brevissimo \n");
-    CPU_ZERO(&cpuset); /*azzero la bitmask*/
-    for(i=0;i<4;i++){
 
-        printf("%lu\n",*array[i]); /*Si lo so è sbagliato, ho fatto così per pescarmi la variabile pthread threadIdN dove N=1,2,3,4 per poter settare
-        la sua affinità e fare la threadjoin*/
-        
-        CPU_SET(i,&cpuset); /*setto il bit della cpu con cui fare affinità*/
-        pthread_create(array[i],NULL,thread_function,NULL); /*indirizzo variabile pthread_t,attributi di default con NULL, istruzioni che dovrà svolgere, parametri da passare in input alla funzione*/
-        
-        
-        pthread_join(threadId1,NULL);/*il ciclo for finisce e lui è il primo sospettato*/
+    printf("numero CPU : %lu\n",sizeof(CPU_COUNT(&cpuset)));
+    printf("INIZIO PROGRAMMA: \n");
 
-        /*Un tempo qui si faceva affinità di cpu, poi però arrivò più di 1 thread :c
+    for(i=0;i<sizeof(CPU_COUNT(&cpuset));i++){
+
+        CPU_ZERO(&cpuset); /*azzero la bitmap (sennò il N-esimo thread prenderà quella dell'N-1esimo che rimarrebbe residua)*/
+        CPU_SET(i,&cpuset); //aggiungo la cpu i-esima al set
+
+        pthread_create(array[i],NULL,thread_function,NULL); /*indirizzo variabile pthread_t,NULL = attributi di default, istruzioni che svolgerà, parametri da passare in input alla funzione*/
         
-        if(pthread_setaffinity_np(threadId,sizeof(&cpuset),&cpuset) == 0){
-            printf("SUCCESSONE\n");
-        };
+        if(pthread_setaffinity_np(*array[i],sizeof(cpuset),&cpuset) == 0){
+            printf("affinità attribuita con successo\n");
+        }
         
-        */
+        if(pthread_join(*array[i],NULL) == 0){ //serve per attendere il l'esecuzione del thread
+            printf("join effettuata con successo\n");
+        }
+        
+        //vediamo quale cpu è affine
+        for(int k = 0; k < 4; k++){ 
+            if(CPU_ISSET(k,&cpuset)){
+                printf("Il thread numero %d è affine alla CPU %d\n",i,k);
+            }
+        }
+
         
 
     }
 
 
-    printf("CICLO FOR BELLO CHE FINITO");
+    printf("Sembra tutto funzionante :  si ringraziano Andrea ed Alessia per il loro aiuto\n");
 
 }
 
 void *thread_function(void*arg){
+
     printf("Ciao sono un nuovo thread e sono appena stato creato,adesso conterò fino a 4\n");
-    for(i=0;i<5;i++){
-        printf("%d\n",i);
+    for(int j=0;j<5;j++){
+        printf("%d\n",j);
         sleep(1);
     }
 
